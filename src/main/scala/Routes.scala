30f27e4c41fe6631c9b71b2a6378a5491f042d45
Reynaldo Gil <gilcu2@gmail.com>
2023-09-09 16:15:27 +0200

    working server

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,3 +1,4 @@
+import cats.data.Kleisli
 import cats.effect.IO
 import org.http4s._
 import org.http4s.dsl.io._
@@ -5,7 +6,7 @@ import org.http4s.dsl.io._
 
 object Routes {
 
-  val routes: HttpRoutes[IO] = HttpRoutes.of {
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "hello" => Ok("Hi")
     case GET -> Root / "hello" / name => Ok(s"Hi $name")
   }
